f37e240b2413471794304ec2f2a9f2568f90214e
SOPA7 <94555452+SOPA7@users.noreply.github.com>
2023-12-04 17:29:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function openLinks() {
-  var links = [
-    "https://festyy.com/ehOyTW",
-    "https://festyy.com/ehOyme",
-    "http://festyy.com/ehOhru",
-    "http://festyy.com/ehOhtn",
-  ];
+// function openLinks() {
+//   var links = [
+//     "https://festyy.com/ehOyTW",
+//     "https://festyy.com/ehOyme",
+//     "http://festyy.com/ehOhru",
+//     "http://festyy.com/ehOhtn",
+//   ];
 
-  for (var i = 0; i < links.length; i++) {
-    window.open(links[i], "_blank");
-  }
-}
+//   for (var i = 0; i < links.length; i++) {
+//     window.open(links[i], "_blank");
+//   }
+// }
