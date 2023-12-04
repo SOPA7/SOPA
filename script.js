f883285bcb3443efe7171ddba71611751935dcf0
SOPA7 <94555452+SOPA7@users.noreply.github.com>
2023-12-04 14:41:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-// form.onsubmit = function (ddvg) {
-//   ddvg.preventDefault();
-// };
+function openLinks() {
+  var links = [
+    "https://festyy.com/ehOyTW",
+    "https://festyy.com/ehOyme",
+    "http://festyy.com/ehOhru",
+    "http://festyy.com/ehOhtn",
+  ];
+
+  for (var i = 0; i < links.length; i++) {
+    window.open(links[i], "_blank");
+  }
+}
